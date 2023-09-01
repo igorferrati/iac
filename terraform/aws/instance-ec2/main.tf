b66368477fe6700fca57a85b66212bea5b5adc25
igorferrati <ferrati.igor@gmail.com>
2023-08-31 23:27:14 -0300

    add ansible

--- terraform/aws/instance-ec2/main.tf
+++ terraform/aws/instance-ec2/main.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-002829755fa238bfa" #amazon-ubuntu
   instance_type = "t2.micro"
   key_name = "ubuntu-oregon"
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo “<h1>Olá mundo via terraform</h1>” > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   
   tags = {
     Name = "Ubuntu-Teste"
